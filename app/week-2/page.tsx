2b3418f3ca6c5d6069f1e1359b9a93ed1618187c
imbananacrazy <bananacrazyness21@gmail.com>
2026-01-22 13:21:12 -0700

    Week 2: All pages and links

---
+++ app/week-2/page.tsx
@@ -0,0 +1,10 @@
+import StudentInfo from "@/components/student.info"
+
+export default function Page() {
+    return (
+        <main>
+            <h1>Shopping List</h1>
+            <StudentInfo />
+        </main>
+    )
+}
---
+++ components/student.info.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Raphael Carriere</p>
+            <Link href="https://github.com/imbananacrazy/cprg306-assignments">GitHub Repository</Link>
+        </div>
+    )
+}
