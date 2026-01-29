11baab74596a59a2379636713ccfddcb9266433d
imbananacrazy <bananacrazyness21@gmail.com>
2026-01-29 16:03:38 -0700

    Proper wrapping

--- app/week-3/page.tsx
+++ app/week-3/page.tsx
@@ -2,11 +2,9 @@ import ItemList from "./item-list"
 
 export default function Page() {
     return (
-        <main className="bg-[#141515] text-white">
-            <div className="flex flex-col items-center justify-center min-h-screen">
-                <h1 className="text-4xl font-bold pb-8">Shopping List</h1>
-                <ItemList />
-            </div>
+        <main className="bg-[#141515] text-white flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold pb-8">Shopping List</h1>
+            <ItemList />
         </main>
     )
 }
