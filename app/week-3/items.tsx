adfb671ffd44ef5d010a42f236202d9102a7114e
imbananacrazy <bananacrazyness21@gmail.com>
2026-01-29 16:02:36 -0700

    Added Interface

--- app/week-3/items.tsx
+++ app/week-3/items.tsx
@@ -1,4 +1,10 @@
-export default function Item({name, quantity, category}: {name: string; quantity: number; category: string}) {
+interface ItemProps {
+    name: string;
+    quantity: number;
+    category: string;
+}
+
+export default function Item({name, quantity, category}: ItemProps) {
     return (
         <li className="font-bold text-white">
             {category.toUpperCase()}: {quantity} {name.charAt(0).toUpperCase() + name.slice(1)}
